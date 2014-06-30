76a85faf95fbb67f99107fcdfd22221c0288b8dc
Peter Nied <pnied@microsoft.com>
2014-06-30 08:56:07 -0700

    figure out what to suppport

--- Licensor/Program.cs
+++ Licensor/Program.cs
@@ -1,15 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Licensor
+﻿namespace Licensor
 {
+    using System;
+    using System.IO;
+    using System.Linq;
+
     class Program
     {
         static void Main(string[] args)
         {
+            if (args.Length != 1)
+            {
+                throw new ApplicationException("Did not see an argument for the directory to apply licenses onto");
+            }
+
+            Directory.EnumerateFiles(args.First(), "*", SearchOption.AllDirectories)
+                .GroupBy(Path.GetExtension)
+                .ToList()
+                .ForEach(k => Console.WriteLine(k.Key, k));
         }
     }
 }
